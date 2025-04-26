8d3ee200494ed3719c830c6c0051d6a0390f67c8
Bun <76037676+BunFirk@users.noreply.github.com>
2025-04-26 20:30:39 +0300

    Update Header.tsx -> bg-fixed!!!
    add bg-fixed

--- components/ui/header/Header.tsx
+++ components/ui/header/Header.tsx
@@ -2,7 +2,7 @@ import { SearchHeader } from "@/components/ui";
 
 export function Header() {
   return (
-   <div className="w-full h-[612px] bg-[url('https://images.unsplash.com/photo-1523755231516-e43fd2e8dca5?q=80&w=1635&auto=format&fit=crop')] bg-cover bg-center">
+   <div className="w-full h-[612px] bg-[url('https://images.unsplash.com/photo-1523755231516-e43fd2e8dca5?q=80&w=1635&auto=format&fit=crop')] bg-fixed bg-cover bg-center">
       <div className="mx-auto py-8 flex justify-center flex-col text-center">
         <h1 className="mx-auto xl:text-9xl lg:text-9xl md:text-9xl font-bona font-bold mt-14 sm:text-9xl text-7xl">Atrium</h1>
         <h2 className="mx-auto font-bona text-[27px]">Your Dream Home, Your Best Invest</h2>
